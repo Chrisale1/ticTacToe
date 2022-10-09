29d427441f00e791a71cb8320a70fa09210eb12e
Christian Ekene Omernna <christiansand91@outlook.com>
2022-10-09 02:30:03 +0000

    structure of some function created

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+document.addEventListener ("DOMContentLoaded", function(){
+    const boxes = document.querySelectorAll('box');
+    const restartButton = document.getElementById("reset")
+
+
+    restartButton.addEventListener('click', resetGame)
+
+});
+
+function resetGame() {
+    
+}
+function endGame() {
+    
+}
+
+function changePlayer() {
+    
+}
+
+function checkWinner(){
+
+}
+
+function checkDraw(){
+    
+}
